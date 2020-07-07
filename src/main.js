0d6d63bd1452151c7a793a211c51d1abf262648a
panfei <604042454@qq.com>
2020-07-07 17:47:08 +0800

    v07071700.2

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/style/common.less'
-import disableScale from '@/util/disableScale'
+import disableScale from '@utils/disableScale'
 
 Vue.config.productionTip = false
 
